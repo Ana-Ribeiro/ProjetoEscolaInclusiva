9a95621509a9225d7765b0c8d48cae23f9d29c63
Ana-Ribeiro <80415309+Ana-Ribeiro@users.noreply.github.com>
2022-05-31 10:46:11 +0100

    upload projeto
    upload projeto

---
+++ EscolaInclusiva/resources/views/layouts/main.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Gestão de Projetos</title>
+
+      
+    </head>
+    
+    
+    <body >
+        @yield('content')
+        <div class="container my-5">
+        <footer
+        
+        class="text-center text-lg-start text-white"
+          style="background-color: #1AA4A6">
+          
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Links -->
+      <section class="">
+     
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contactos</h6>
+            <p><i class="fas fa-home mr-3"></i> ESTG|IPVC</p>
+            <p><i class="fas fa-envelope mr-3"></i> Avenida do Atlântico, 644</p>
+            <p><i class="fas fa-phone mr-3"></i> 4900-348 Viana do Castelo</p>
+            <p><i class="fas fa-print mr-3"></i> escolainclusiva@estg.ipvc.pt</p>
+            <p><i class="fas fa-print mr-3"></i> (+351) 258 819 700</p>
+
+          </div>
+         
+
+      
+ 
+
+ 
+       
+        </footer>
+        </div>
+    
+    </body>
+</html>
+
+
